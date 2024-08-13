d3c2bdf5f1282452ec084516ca042d68ecd65044
Jason Carey <jasdcarey@gmail.com>
2024-08-13 14:03:43 +1000

    temp

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # Chinese Handwritten Character Classification
 
-Dataset taken from [CASIA Online and Offline Chinese Handwriting Databases](https://nlpr.ia.ac.cn/databases/handwriting/Download.html)
+Dataset taken from [CASIA Online and Offline Chinese Handwriting Databases](https://nlpr.ia.ac.cn/databases/handwriting/Download.html).
+
+A research paper outlining how the samples were produced can be found [here](https://nlpr.ia.ac.cn/databases/download/ICDAR2011-CASIA%20databases.pdf)
 
 We use GB2312 encoding for Chinese characters, where each is represented by a pair of bytes.
+
+## Accuracy Data
+
+| Model Type    | Model Config                              | Epochs | Accuracy |
+| ------------- | ----------------------------------------- | ------ | -------- |
+| CNN           | 3 convolutional, 2 fully connected layers | 10     | 83.00%   |
+| a             | a                                         | a      | a        |
--- main.py
+++ main.py
@@ -10,6 +10,8 @@ import torch.nn.functional as F
 import torch.optim as optim
 from sklearn.preprocessing import LabelEncoder
 
+device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+
 
 # Load and parse
 def read_gnt_file(file_path):
@@ -29,18 +31,49 @@ def read_gnt_file(file_path):
                 break
 
 
-# Preprocess
-transform = transforms.Compose(
-    [
-        transforms.Resize((64, 64)),
-        transforms.ToTensor(),
-    ]
-)
+def calculate_padding(height, width, target_height=64, target_width=64):
+    scale = min(target_height / height, target_width / width)
+    scaled_height = int(height * scale)
+    scaled_width = int(width * scale)
+
+    padding_top = (target_height - scaled_height) // 2
+    padding_bottom = target_height - scaled_height - padding_top
+    padding_left = (target_width - scaled_width) // 2
+    padding_right = target_width - scaled_width - padding_left
+
+    return padding_left, padding_right, padding_top, padding_bottom
+
+
+def create_transform(height, width, target_size=(64, 64)):
+    target_height, target_width = target_size
+    padding_left, padding_top, padding_right, padding_bottom = calculate_padding(
+        height, width, target_height, target_width
+    )
+
+    transform = transforms.Compose(
+        [
+            transforms.Pad(
+                (padding_left, padding_top, padding_right, padding_bottom), fill=255
+            ),
+            transforms.Resize((64, 64)),
+            # transforms.RandomRotation(10),
+            # transforms.RandomHorizontalFlip(),
+            # transforms.RandomAffine(0, translate=(0.1, 0.1)),
+            transforms.ToTensor(),
+            # transforms.Normalize(mean=[0.5], std=[0.5]),
+        ]
+    )
+
+    return transform
 
 
 def preprocess_image(image):
+    height, width = image.shape
     image = Image.fromarray(image)
-    return transform(image)
+    transform = create_transform(height, width, (64, 64))
+    image = transform(image)
+
+    return image
 
 
 class HWDBDataset(Dataset):
@@ -62,8 +95,6 @@ class HWDBDataset(Dataset):
     def __getitem__(self, idx):
         image = self.data[idx]
         label = self.labels[idx]
-        # label = label.encode("gb2312")
-        # label = int.from_bytes(label, byteorder="big")
         return image, label
 
 
@@ -77,18 +108,23 @@ class SimpleCNN(nn.Module):
     def __init__(self, num_classes):
         super(SimpleCNN, self).__init__()
         self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
         self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
         self.pool = nn.MaxPool2d(2, 2)
         self.fc1 = nn.Linear(128 * 8 * 8, 256)
+        self.dropout = nn.Dropout(0.4)
         self.fc2 = nn.Linear(256, num_classes)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = self.pool(F.relu(self.conv3(x)))
+        x = self.pool(F.relu(self.bn1(self.conv1(x))))
+        x = self.pool(F.relu(self.bn2(self.conv2(x))))
+        x = self.pool(F.relu(self.bn3(self.conv3(x))))
         x = x.view(-1, 128 * 8 * 8)
         x = F.relu(self.fc1(x))
+        x = self.dropout(x)
         x = self.fc2(x)
         return x
 
@@ -98,7 +134,7 @@ def train(model, train_loader, test_loader, criterion, optimizer, num_epochs=10)
         model.train()
         running_loss = 0.0
         for images, labels in train_loader:
-            labels = labels.long()
+            images, labels = images.to(device), labels.to(device).long()
             optimizer.zero_grad()
             outputs = model(images)
             loss = criterion(outputs, labels)
@@ -112,7 +148,7 @@ def train(model, train_loader, test_loader, criterion, optimizer, num_epochs=10)
         total = 0
         with torch.no_grad():
             for images, labels in test_loader:
-                labels = labels.long()
+                images, labels = images.to(device), labels.to(device).long()
                 outputs = model(images)
                 _, predicted = torch.max(outputs.data, 1)
                 total += labels.size(0)
@@ -134,7 +170,13 @@ if __name__ == "__main__":
 
     num_classes = len(train_dataset.label_encoder.classes_)
 
-    model = SimpleCNN(num_classes=num_classes)
+    for i in range(10):
+        image, label = train_dataset[i]
+        print(f"Image size (HxW): {image.size(1)}x{image.size(2)}")
+        image_pil = transforms.ToPILImage()(image.squeeze(0))
+        image_pil.show()
+
+    model = SimpleCNN(num_classes=num_classes).to(device)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=0.001)
 
